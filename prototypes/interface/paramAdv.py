e777d98469ba0615d5577b56f8b2102e130565c6
Mael36 <mael.beltrame@free.fr>
2024-10-25 14:03:33 +0200

    Add files via upload

---
+++ prototypes/interface/paramAdv.py
@@ -0,0 +1,50 @@
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSpinBox, QPushButton, QComboBox
+
+class ParamAdvWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Paramètres Avancés')
+        self.setGeometry(150, 150, 400, 400)  # Définir la taille de la fenêtre
+
+        # Layout principal
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        # Taille du batch
+        batch_size_label = QLabel('Taille du batch :', self)
+        layout.addWidget(batch_size_label)
+        
+        self.batch_size_spin = QSpinBox(self)
+        self.batch_size_spin.setRange(1, 128)  # Plage de valeurs
+        layout.addWidget(self.batch_size_spin)
+
+        # Taux d'apprentissage
+        learning_rate_label = QLabel('Taux d\'apprentissage :', self)
+        layout.addWidget(learning_rate_label)
+        
+        self.learning_rate_spin = QSpinBox(self)
+        self.learning_rate_spin.setRange(1, 100)  # Plage de valeurs
+        self.learning_rate_spin.setSingleStep(1)
+        layout.addWidget(self.learning_rate_spin)
+
+        # Nombre d'époques
+        epochs_label = QLabel('Nombre d\'époques :', self)
+        layout.addWidget(epochs_label)
+        
+        self.epochs_spin = QSpinBox(self)
+        self.epochs_spin.setRange(1, 1000)  # Plage de valeurs
+        self.epochs_spin.setSingleStep(10)
+        layout.addWidget(self.epochs_spin)
+
+        # Type d'optimiseur
+        optimizer_label = QLabel('Optimiseur :', self)
+        layout.addWidget(optimizer_label)
+
+        self.optimizer_combo = QComboBox(self)
+        self.optimizer_combo.addItems(['Adam', 'SGD', 'RMSprop'])  # Options d'optimiseur
+        layout.addWidget(self.optimizer_combo)
+
+        # Bouton pour revenir en mode normal
+        self.normal_mode_button = QPushButton('Revenir en mode normal', self)
+        self.normal_mode_button.clicked.connect(self.close)  # Fermer la fenêtre
+        layout.addWidget(self.normal_mode_button)
